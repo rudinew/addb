ee2b2344d82b110fa5b9a253231a00b6446e83ea
Nadiia <rudi_777@ukr.net>
2018-05-07 14:01:22 +0300

    commit1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=index.html">
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+      <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+<title>Автомобільні дороги і дорожнє будівництво</title>
+<script language="javascript">
+    window.location.href = "index.html"
+</script>
+</head>
+<body>
+Go to <a href="index.html">index.html</a>
+</body>
+</html>
+
